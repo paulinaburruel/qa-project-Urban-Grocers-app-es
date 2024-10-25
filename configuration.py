3add63f0ac32fdf2b02f7d0209e23e9e5f5a1787
Paulina Burruel <paulina.burruel@gmail.com>
2024-10-24 18:58:02 -0700

    Segundo commit del Proyecto 7

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = 'https://cnt-86d90f59-97ba-4403-b94e-84fe44e24e9d.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-a7e9c49a-e177-4f93-82a0-12c60c753c18.containerhub.tripleten-services.com'
 CREATE_USER_PATH = '/api/v1/users'
 KITS_PATH = '/api/v1/kits'
 
